612970049c3adc71367355a48508a68b046d27a6
petko.lyutskanov <simona2000petko@abv.bg>
2023-03-04 13:44:26 +0200

    Request JSON data from openweathermap api with Volley

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -9,6 +9,19 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     Button btn_cityID, btn_getWeatherByID, btn_getWeatherByName;
@@ -30,7 +43,36 @@ public class MainActivity extends AppCompatActivity {
         btn_cityID.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "You clicked me 1!", Toast.LENGTH_SHORT).show();
+                // Instantiate the RequestQueue.
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                String url = "https://api.openweathermap.org/data/2.5/weather?q="+ et_dataInput.getText().toString() + "&appid=5d5affe841dd964f4563f5c3771df5d5";
+
+                JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        JSONArray weatherJSON;
+                        JSONObject mainJSON, windJSON;
+                        Integer visibility;
+
+                        try {
+                            weatherJSON = response.getJSONArray("weather");
+                            mainJSON = response.getJSONObject("main");
+                            visibility = response.getInt("visibility");
+                            windJSON = response.getJSONObject("wind");
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+
+                        Toast.makeText(MainActivity.this, weatherJSON.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Error occurred", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                queue.add(request);
             }
         });
 
